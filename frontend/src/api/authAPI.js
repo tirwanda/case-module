e4e0cc97c13f3269509d6504260c9f7ba333889b
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2024-01-30 20:16:27 +0700

    Integrate Firebase

--- frontend/src/api/authAPI.js
+++ frontend/src/api/authAPI.js
@@ -1,8 +1,14 @@
 import axios from "axios";
 
+const configHeaders = {
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+  },
+};
+
 export const userSignIn = (userData) =>
-  axios.post(`${process.env.REACT_APP_HOST_URL}/api/v1/signin`, userData, {
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-  });
+  axios.post(
+    `https://us-central1-case-module.cloudfunctions.net/server/api/v1/signin`,
+    userData,
+    configHeaders
+  );
