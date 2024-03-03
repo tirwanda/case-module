690ce548250fc514bad849d8a00e3f54c787951f
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2024-03-03 14:07:58 +0700

    Fix URL

--- frontend/src/api/URL.js
+++ frontend/src/api/URL.js
@@ -1,2 +1,2 @@
-// export const HOST_URL = "https://us-central1-case-module.cloudfunctions.net/server";
-export const HOST_URL = "http://localhost:8000";
+export const HOST_URL = "https://us-central1-case-module.cloudfunctions.net/server";
+// export const HOST_URL = "http://localhost:8000";
